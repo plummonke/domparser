7c0da03f3d94cf00636b8da4e7cbe5ec327f16f8
Sean M Tullis <sean.tullis@usda.gov>
2019-11-27 13:26:10 -0700

    rename: parser.py changed to myparser.py to correct a module name conflict.
    update: Parser updated to handle more HTML tags.

--- myparser.py
+++ myparser.py
@@ -10,8 +10,13 @@ class Parser(hp.HTMLParser):
         self.root.add_child(tag, **dict(attrs))
 
     def handle_endtag(self, tag):
-        self.root.close()
+        self.root.close_tag()
 
     def handle_data(self, content):
         self.root.add_child("text", data=content)
 
+    def handle_comment(self, data):
+        self.root.add_child("comment", data=data)
+
+    def handle_decl(self, decl):
+        self.root.add_child("doc", data=decl)
